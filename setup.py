df9d4e65d0c90c57f906ed6523b7719dfaebfcb5
Revar Desmera <revarbat@gmail.com>
2024-01-25 22:22:30 -0800

    Move to pyproject,toml

--- setup.py
+++ setup.py
@@ -1,50 +1,8 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-VERSION = "1.1.1"
+if __name__ == "__main__":
+    setuptools.setup()
 
-
-with open('README.rst') as f:
-    LONG_DESCR = f.read()
-
-data_files = []
-
-setup(
-    name='openscad_runner',
-    version=VERSION,
-    description='A Python library to interface with the OpenSCAD app.',
-    long_description=LONG_DESCR,
-    long_description_content_type='text/x-rst',
-    author='Revar Desmera',
-    author_email='revarbat@gmail.com',
-    url='https://github.com/revarbat/openscad_runner',
-    download_url='https://github.com/revarbat/openscad_runner/archive/v{}.zip'.format(VERSION),
-    packages=['openscad_runner'],
-    license='MIT License',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Manufacturing',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3',
-        'Topic :: Artistic Software',
-        'Topic :: Multimedia :: Graphics :: 3D Modeling',
-        'Topic :: Multimedia :: Graphics :: 3D Rendering',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    keywords='openscad interface',
-    install_requires=[
-        'setuptools',
-        'Pillow>=7.2.0',
-        'pygifsicle>=1.0.2',
-        'apng>=0.3.4'
-    ],
-    data_files=data_files,
-)
 
